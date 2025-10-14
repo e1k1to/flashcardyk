05dc0ade7d85f2fc219c7b0a99317bea6d0d7904
e1k1to <eikimillian@gmail.com>
2025-10-14 17:09:45 -0300

    arquivos comecinho

---
+++ flashcardo/Models/Card.cs
@@ -0,0 +1,25 @@
+﻿namespace flashcardo;
+
+public class Card
+{
+    public bool IsFlipped = false;
+    public string TextFront { get; set; }
+    public string TextBack { get; set; }
+    
+    public Card(string textFront, string textBack) 
+    {
+        TextFront = textFront;
+        TextBack = textBack;
+    }
+
+    public void FlipCard()
+    {
+        IsFlipped = !IsFlipped;
+    }
+
+    public override string ToString()
+    {
+        return $"{TextFront}:{TextBack}";
+    }
+
+}
---
+++ flashcardo/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace flashcardo;
+
+public class Program
+{
+
+    const string flashCardFilePath = "example.txt"; //what about flsc
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+
+
+    // Read from TXT
+    // Words separates by TABS
+    public static List<Card> readCardsFromFile(string filePath)
+    {
+        List<Card> cardList = new List<Card>();
+        string[] linhas = [];
+
+        try
+        {
+            linhas = File.ReadAllLines(filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Unable to open file: ");
+            Console.WriteLine(ex.Message);
+        }
+        
+        foreach(string linha in linhas)
+        {
+            string[] words = linha.Split("\t");
+
+            if(words.Length < 2)
+            {
+                throw new Exception("Error on loading cards, check your file for errors.");
+            }
+
+            string textFront = words[0];
+            string textBack = words[1];
+
+            cardList.Add(
+                new Card(textFront:textFront, textBack:textBack)
+            );
+        }
+
+        return cardList;
+    }
+
+    // Save to TXT
+    //
+    public static bool saveToTXT() 
+    {
+        return false; 
+    }
+
+}
